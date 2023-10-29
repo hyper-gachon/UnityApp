5f00a161b96565b29d222aed691df794db7773fd
Hangyeol-SSeo <dongha313@gmail.com>
2023-10-29 14:37:09 +0900

    공지사항 띄우기

--- Assets/button.cs
+++ Assets/button.cs
@@ -1,13 +1,51 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
+using UnityEngine.Networking;
 
-public class ButtonClickHandler : MonoBehaviour, IPointerClickHandler
+public class ButtonHandler : MonoBehaviour
 {
-    public Button button;
+    public Button NoticeButton;
+    public Button AcademicScheduleButton;
+    public Button HomeworkButton;
+    public Button BusTimetableButton;
 
-    public void OnPointerClick(PointerEventData eventData)
+    void Start()
     {
-        Debug.Log("Button clicked");
+        NoticeButton.onClick.AddListener(() => SendQuery("Panel_Notice"));
+        AcademicScheduleButton.onClick.AddListener(() => SendQuery("Panel_Academy"));
+        HomeworkButton.onClick.AddListener(() => SendQuery("Panel_Homework"));
+        BusTimetableButton.onClick.AddListener(() => SendQuery("Panel_Bus"));
+    }
+
+    void SendQuery(string panelName)
+    {
+        string url = "localhost:3000";
+        if (panelName.Equals("Panel_Notice"))
+            url = url + "/crawling/fullnotice";
+        else if (panelName.Equals("Panel_Academy"))
+            url = url + "/crawling/academic_schedule";
+        else if (panelName.Equals("Panel_Homework"))
+            url = url + "/crawling/cybercampus";
+        else if (panelName.Equals("Panel_Bus"))
+            url = url + "/crawling/bus";
+
+        StartCoroutine(SendRequest(url));
+    }
+
+    System.Collections.IEnumerator SendRequest(string url)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Response: " + request.downloadHandler.text);
+            }
+            else
+            {
+                Debug.LogError("Request failed: " + request.error);
+            }
+        }
     }
 }
--- Assets/show_menu.cs
+++ Assets/show_menu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Networking;
+using TMPro; 
 
 public class ToggleTextPanel : MonoBehaviour
 {
     public GameObject textPanel;
+    public TextMeshProUGUI responseTextMeshPro;  // TextMeshProUGUI 컴포넌트에 대한 참조
 
     private void Start()
     {
@@ -16,5 +17,45 @@ public class ToggleTextPanel : MonoBehaviour
     private void TogglePanel()
     {
         textPanel.SetActive(!textPanel.activeSelf);
+        if (textPanel.activeSelf)
+        {
+            StartCoroutine(SendRequest(textPanel.name));
+
+        }
+    }
+
+    private IEnumerator SendRequest(string panelName)
+    {
+        string url = "localhost:8080";
+        if (panelName.Equals("Panel_Notice"))
+            url = url + "/crawling/fullnotice";
+        else if (panelName.Equals("Panel_Academy"))
+            url = url + "/crawling/academic_schedule";
+        else if (panelName.Equals("Panel_Homework"))
+            url = url + "/crawling/cybercampus";
+        else if (panelName.Equals("Panel_Bus"))
+            url = url + "/crawling/bus";
+
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log(request.downloadHandler.text);
+                if (responseTextMeshPro != null)
+                {
+                    responseTextMeshPro.text = request.downloadHandler.text;  // 텍스트 업데이트
+                }
+                else
+                {
+                    Debug.LogError("TextMeshProUGUI component is not set.");
+                }
+            }
+            else
+            {
+                Debug.LogError("Request failed: " + request.error);
+            }
+        }
     }
 }
